ad16dc4e5cdee41a357f78fe214adec2d39122a4
workswithweb <workswithweb@gmail.com>
2016-06-21 07:13:11 -0500

    open chrome app as single instance

--- chrome/js/background_chrome.js
+++ chrome/js/background_chrome.js
@@ -1,5 +1,7 @@
 chrome.app.runtime.onLaunched.addListener(function(launchData) {
     chrome.app.window.create('index.html', {
+        singleton: true,
+        id: 'MQTTBox',
         'bounds': {
             'width': Math.round(window.screen.availWidth),
             'height': Math.round(window.screen.availHeight)
